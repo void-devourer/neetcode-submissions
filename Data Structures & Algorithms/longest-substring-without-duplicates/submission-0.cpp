2cb51dc6cb1a9ffcfe0c78dedbdcc12c4567e70f
Piyush Kumar Gupta <piyush.kumar.24063@iitgoa.ac.in>
2026-07-06 06:15:21 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> c;
+        int l = 0, res = 0;
+        for(int r=0;r<s.size();r++){
+            while(c.find(s[r])!=c.end()){
+                c.erase(s[l]);
+                l++;
+            }
+            c.insert(s[r]);
+            res = max(res, r-l+1);
+        }
+        return res;
+    }
+};
