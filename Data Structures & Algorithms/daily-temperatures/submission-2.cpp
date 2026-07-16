6bceb455acfbe04f7fb54391e0be8fde447c5328
Piyush Kumar Gupta <piyush.kumar.24063@iitgoa.ac.in>
2026-07-16 20:39:15 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        vector<int> res(temperatures.size(), 0);
+        for (int i = 0; i < temperatures.size(); i++) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+            int prev = st.top();
+            st.pop();
+            res[prev] = i - prev;
+            }
+            st.push(i);
+        }
+        return res;
+    }
+};
