173a0e5e3f1c8b7a57dd92001fab0efeefe1ccc0
Piyush Kumar Gupta <piyush.kumar.24063@iitgoa.ac.in>
2026-07-18 00:50:49 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int x:stones){
+            pq.push(x);
+        }
+        while(pq.size()>1){
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            if(b<a){
+                pq.push(a-b);
+            }
+        }
+        pq.push(0);
+        return pq.top();
+    }
+};
